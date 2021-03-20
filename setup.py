d3dbaaf4486c97fbc8251659195c4a223b329657
Vaanathi Sundaresan <vaanathi.sundaresan@ndcn.ox.ac.uk>
2021-03-20 16:41:28 +0000

    setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup,find_packages
+with open('requirements.txt', 'rt') as f:
+    install_requires = [l.strip() for l in f.readlines()]
+
+setup(name='truenet',
+	version='1.0.1',
+	description='DL method for WMH segmentation',
+	author='Vaanathi Sundaresan',
+	install_requires=install_requires,
+    scripts=['truenet/scripts/truenet'],
+	packages=find_packages(),
+	include_package_data=True)
