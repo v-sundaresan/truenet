d97094433e275cec120d9c889f41d49a6b2a112a
Paul McCarthy <pauldmccarthy@gmail.com>
2024-07-24 17:55:47 +0100

    Remove debugging statements which cause error (see #9, #10)

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ with open('requirements.txt', 'rt') as f:
     install_requires = [l.strip() for l in f.readlines()]
 
 setup(name='truenet',
-	version='1.0.0',
+	version='1.0.1',
 	description='DL method for WMH segmentation',
 	author='Vaanathi Sundaresan',
 	install_requires=install_requires,
